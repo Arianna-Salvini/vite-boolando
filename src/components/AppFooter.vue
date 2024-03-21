f1eb14bfc2ea72b930b10fd537d814825c316a78
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-21 19:23:19 +0100

    Add footer html structure in AppFooter.vue and syle in partial scss in style sheet scss

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,11 +7,32 @@ export default {
 </script>
 
 <template>
-    <div>footer</div>
+    <footer>
+        <div class="footer_container">
+            <div class="footer_left">
+                <div class="name_srl">Boolando s.r.l.</div>
+                <div class="infos">
+                    <ul class="infos">
+                        <li class="unodered_list_footer">Informazioni legali</li>
+                        <li class="unodered_list_footer">Informazioni legali</li>
+                        <li class="unodered_list_footer">Diritto di recesso</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="footer_right">
+                <div class="find_us">Trovaci anche su</div>
+                <div class="social_icons">
+                    <i class="fa-brands fa-x-twitter"></i>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-youtube "></i>
+                    <i class="fa-brands fa-square-pinterest"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                </div>
+            </div>
+        </div>
+
+    </footer>
 </template>
 
-<style>
-footer {
-    margin-top: 2rem;
-}
-</style>
+<style></style>
