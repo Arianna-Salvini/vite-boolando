ee82ed452763c9fe93a7ce1d5920ffdc8cfec312
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-22 12:07:01 +0100

    Fix name of ProductCard component and add to App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooter from './components/AppFooter.vue';
+import ProductCars from './components/ProductCard.vue'
 
 export default {
   name: 'App',
@@ -10,6 +11,7 @@ export default {
     AppHeader,
     AppMain,
     AppFooter,
+    ProdoctCard,
 
   }
 }
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,13 @@
+<script>
+
+export default {
+    name: 'ProdoctCard'
+}
+
+</script>
+
+<template>
+
+</template>
+
+<style></style>
