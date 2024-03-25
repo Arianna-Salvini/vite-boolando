3693db0a727f136ff35b45dbd2efebcbb473fcd8
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-03-25 17:37:39 +0100

    Get product from api

--- src/state.js
+++ src/state.js
@@ -1,6 +1,17 @@
 import { reactive } from "vue";
+import axios from 'axios'
 
 export const state = reactive({
 
-    message: 'this is a test'
+    products: [],
+    products_api_url: 'http://localhost:3000/products',
+
+    getProducts(url) {
+        axios.get(url)
+            .then(response => {
+                this.products = response.data
+            })
+
+
+    }
 })
